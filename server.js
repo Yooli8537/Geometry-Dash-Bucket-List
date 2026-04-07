2e963de606b194e32240ef73986835d8cd237c44
Yooli8537 <Yooli8537@gmx.ch>
2026-04-07 22:12:37 +0200

    Auto-create data.json if not exists + Code Comments

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// If data.json is missing, it creates an empty data.json file. Main use is for a fresh close of the app.
+if (!fs.existsSync("data.json")) {
+  fs.writeFileSync("data.json", "[]");
+}
+
+// GET request
 app.get('/data', (req, res) => {
   const data = fs.readFileSync('data.json', 'utf-8');
   res.json(JSON.parse(data));
 });
 
+// POST request
 app.post('/data', (req, res) => {
   const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
   data.push(req.body);
@@ -21,6 +28,7 @@ app.post('/data', (req, res) => {
   res.json({ success: true });
 });
 
+// Confirmation message
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
