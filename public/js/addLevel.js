217fdaa56bcbcfe721c72cf06350ce01fbcc65ac
Yooli8537 <Yooli8537@gmx.ch>
2026-04-07 13:30:52 +0200

    Selecting traits functionality & Styling changes for homepage

--- public/js/addLevel.js
+++ public/js/addLevel.js
@@ -1,1 +1,13 @@
-const addForm = document.querySelector("#addLevelForm")
+const addForm = document.querySelector("#addLevelForm");
+const traitSelector = document.querySelector("#traitSelector");
+const submitButton = document.querySelector("#submitLevelButton");
+
+document.querySelectorAll(".traitOption").forEach((trait) => {
+  trait.addEventListener("click", () => {
+    trait.classList.toggle("selected");
+  });
+});
+
+submitButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+});
