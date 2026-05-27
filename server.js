d7b5c882fcc04607466e792083088619c8374acb
Yooli8537 <Yooli8537@gmx.ch>
2026-05-27 14:20:27 +0200

    Edited Port to be in line with Webapp Ecosystem

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 6000;
+const PORT = 8564;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
